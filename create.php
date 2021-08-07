e8d37c18d3745d753882a40f18d12c141439f28c
ameenalkaisi <ameen.alkaisi01@gmail.com>
2021-08-07 14:11:54 -0400

    changed indentation on profile and create pages

--- create.php
+++ create.php
@@ -1,106 +1,106 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <title>Blogsite</title>
-    <!-- meta tags -->
-    <meta charset="utf-8">
-    <meta name="keywords" content="blog, blogsite, create blogpost" />
-    <meta name="description" content="Blog hosting service and search" />
+	<head>
+		<title>Blogsite</title>
+		<!-- meta tags -->
+		<meta charset="utf-8">
+		<meta name="keywords" content="blog, blogsite, create blogpost" />
+		<meta name="description" content="Blog hosting service and search" />
 
-    <link rel="stylesheet" href="css/styles.css" />
-  </head>
-  <body>
-    <?php 
-        session_start();
-        include_once('navbar.php'); 
-    ?>
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body>
+		<?php 
+			session_start();
+			include_once('navbar.php'); 
+		?>
 
-    <!--This is the textarea input that will allow users to insert simple
-    text blogs.-->
-    <div class="create">
-      <div class="createLeft">
+		<!--This is the textarea input that will allow users to insert simple
+		text blogs.-->
+		<div class="create">
+			<div class="createLeft">
 
-      </div>
-      <div class="createmiddle">
-        <div class="createintro">
-          <h1>Welcome to the Create page!</h1>
-          <p>This is where you can post blogs and attach tags! you may also save
-          your work as a draft to return to it later. If you wish to start, make
-          sure you are logged in and start writing whatever you wish. You can add
-          tags to the blog later.</p>
+			</div>
+			<div class="createmiddle">
+				<div class="createintro">
+				<h1>Welcome to the Create page!</h1>
+				<p>This is where you can post blogs and attach tags! you may also save
+				your work as a draft to return to it later. If you wish to start, make
+				sure you are logged in and start writing whatever you wish. You can add
+				tags to the blog later.</p>
 
-          <form id="createform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
-            Article Title:<input type="text" name="title" size="100">
-            <!--Temporary measure to post the blog under a particular name. Just center
-            a real display name the php fetches its user id to link them properly.-->
-            Username:<input type="text" name="user" size="100">
-            <textarea form="createform" name="textPost" id="createText"></textarea>
-            <br>
-            Article tags:<input type="text" name="tags" size="100">
-            <br>
-            <input name ="send" type="submit" value="Post Article">
-          </form>
-        </div>
-      </div>
-      <div class="createright">
-      </div>
-    </div>
-    <?php
-      //conect to db.
-      require_once('connection.php');
-      require_once('utils.php');
+				<form id="createform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+					Article Title:<input type="text" name="title" size="100">
+					<!--Temporary measure to post the blog under a particular name. Just center
+					a real display name the php fetches its user id to link them properly.-->
+					Username:<input type="text" name="user" size="100">
+					<textarea form="createform" name="textPost" id="createText"></textarea>
+					<br>
+					Article tags:<input type="text" name="tags" size="100">
+					<br>
+					<input name ="send" type="submit" value="Post Article">
+				</form>
+				</div>
+			</div>
+			<div class="createright">
+			</div>
+		</div>
+		<?php
+			//conect to db.
+			require_once('connection.php');
+			require_once('utils.php');
 
-      $db = db_connect();
+			$db = db_connect();
 
-      //form hookup for, handles the title and tags, makes them more usable for
-      //the current db set
-      //TODO: The tage system for the DB is to simple, we need a table that every
-      //Blog carries a FK to to store them in a way that is much easier to search.
-      if($_SERVER["REQUEST_METHOD"] == "POST"){
-          $send=0;
-          if(!empty($_POST["title"])){
-            $title = input($_POST["title"]);
-          } else {
-            $send=1;
-          }
-          if(!empty($_POST["textPost"])){
-            $textPost = input($_POST["textPost"]);
-          } else {
-            $send=1;
-          }
-          if(!empty($_POST["user"])){
-            $user = input($_POST["user"]);
-          } else {
-            $send=1;
-          }
-          $tags = input($_POST["tags"]);
-          $tagsSearchReady = str_replace(' ','',$tags);
-          $useridSend="";
+			//form hookup for, handles the title and tags, makes them more usable for
+			//the current db set
+			//TODO: The tage system for the DB is to simple, we need a table that every
+			//Blog carries a FK to to store them in a way that is much easier to search.
+			if($_SERVER["REQUEST_METHOD"] == "POST"){
+				$send=0;
+				if(!empty($_POST["title"])){
+					$title = input($_POST["title"]);
+				} else {
+					$send=1;
+				}
+				if(!empty($_POST["textPost"])){
+					$textPost = input($_POST["textPost"]);
+				} else {
+					$send=1;
+				}
+				if(!empty($_POST["user"])){
+					$user = input($_POST["user"]);
+				} else {
+					$send=1;
+				}
+				$tags = input($_POST["tags"]);
+				$tagsSearchReady = str_replace(' ','',$tags);
+				$useridSend="";
 
-          $queryResult = $db->query("SELECT userid FROM users WHERE display_name='$user';");
-          while($userid = $queryResult->fetch_assoc()){
-            $useridSend = $userid["userid"];
-          }
+				$queryResult = $db->query("SELECT userid FROM users WHERE display_name='$user';");
+				while($userid = $queryResult->fetch_assoc()){
+					$useridSend = $userid["userid"];
+				}
 
-          if($send==0){
-            $sql = "INSERT INTO posts (userid ,title, content, tags) VALUES ('$useridSend','$title','$textPost','$tagsSearchReady')";
-            if ($db->query($sql) === TRUE) {
-              echo "NEW POST MADE<br>";
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-          }
-          $send=0;
-      }
+				if($send==0){
+					$sql = "INSERT INTO posts (userid ,title, content, tags) VALUES ('$useridSend','$title','$textPost','$tagsSearchReady')";
+					if ($db->query($sql) === TRUE) {
+						echo "NEW POST MADE<br>";
+					} else {
+						echo "Error: " . $sql . "<br>" . $conn->error;
+					}
+				}
+				$send=0;
+			}
 
-      mysqli_close($db);
+			mysqli_close($db);
 
-      function input($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-      }
-    ?>
-  </body>
+			function input($data){
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}
+		?>
+	</body>
 </html>
--- profile.php
+++ profile.php
@@ -1,67 +1,67 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <title>Blogsite</title>
-    <!-- meta tags -->
-    <meta charset="utf-8">
-    <meta name="keywords" content="blog, blogsite, create blogpost" />
-    <meta name="description" content="Blog hosting service and search" />
+	<head>
+		<title>Blogsite</title>
+		<!-- meta tags -->
+		<meta charset="utf-8">
+		<meta name="keywords" content="blog, blogsite, create blogpost" />
+		<meta name="description" content="Blog hosting service and search" />
 
-    <link rel="stylesheet" href="css/styles.css" />
-  </head>
-  <body>
-    <?php 
-      session_start();
-      include_once("navbar.php");
-    ?>
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body>
+		<?php 
+			session_start();
+			include_once("navbar.php");
+		?>
 
-    <h1>Skeleton Profile, I needed this for testing create, rebase as needed.</h1>
-    <form id="createform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
-      User name:<input type="text" name="name" size="100">
-      <br>
-      <select name="admin">
-        <option value="1">Yes</option>
-        <option value="0">No</option>
-      </select>
-      <input name ="send" type="submit" value="Post Article">
-    </form>
-    <?php
-      require_once('connection.php');
-      require_once('utils.php');
+		<h1>Skeleton Profile, I needed this for testing create, rebase as needed.</h1>
+		<form id="createform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+			User name:<input type="text" name="name" size="100">
+			<br>
+				<select name="admin">
+				<option value="1">Yes</option>
+				<option value="0">No</option>
+			</select>
+			<input name ="send" type="submit" value="Post Article">
+		</form>
+		<?php
+		require_once('connection.php');
+		require_once('utils.php');
 
-      $db = db_connect();
+		$db = db_connect();
 
-      if($_SERVER["REQUEST_METHOD"] == "POST"){
-          //check if user exist by searching name
-          $send=0;
-          if(!empty($_POST["name"])){
-            $name = input($_POST["name"]);
-          } else {
-            $send=1;
-          }
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+			//check if user exist by searching name
+			$send=0;
+			if(!empty($_POST["name"])){
+				$name = input($_POST["name"]);
+			} else {
+				$send=1;
+			}
 
-          $admin = input($_POST["admin"]);
+			$admin = input($_POST["admin"]);
 
-          //validates the new user so as not to enter empty data
-          if($send==0){
-            $sql = "INSERT INTO users (display_name, is_admin) VALUES ('$name','$admin')";
-            if ($db->query($sql) === TRUE) {
-              echo "NEW POST MADE<br>";
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-          }
-          $send=0;
-      }
+			//validates the new user so as not to enter empty data
+			if($send==0){
+				$sql = "INSERT INTO users (display_name, is_admin) VALUES ('$name','$admin')";
+				if ($db->query($sql) === TRUE) {
+					echo "NEW POST MADE<br>";
+				} else {
+					echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+			}
+			$send=0;
+		}
 
-      mysqli_close($db);
+		mysqli_close($db);
 
-      function input($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-      }
-    ?>
-  </body>
+		function input($data){
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+		?>
+	</body>
 </html>
